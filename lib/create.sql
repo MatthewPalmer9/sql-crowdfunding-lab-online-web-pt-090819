661edde3e1790660d99f41700458de272800e2ef
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-10 03:41:17 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,5 +3,19 @@ CREATE TABLE project (
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge (
+    id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    project_id INTEGER
 );
